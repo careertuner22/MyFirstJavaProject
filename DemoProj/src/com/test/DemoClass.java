b2d72154b85f241b90947306f0711f11062a1ed3
careertuner22 <admin@careertuner.io>
2022-11-03 08:48:21 -0400

    this is first sample demojava file

---
+++ DemoProj/src/com/test/DemoClass.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author kyreddy
+ *
+ */
+public class DemoClass {
+
+	/**
+	 * 
+	 */
+	public DemoClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing");
+
+	}
+
+}
